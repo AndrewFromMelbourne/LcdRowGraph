9fdc34811ff877c6af363a458701745e691f2141
Andrew Duncan <arduncan@bigpond.com>
2012-01-24 22:16:01 +1100

    Added setColumns() function to allow the start and end column of the graph to be changed.

--- LcdRowGraph.cpp
+++ LcdRowGraph.cpp
@@ -13,20 +13,20 @@
 //-------------------------------------------------------------------------
 
 LcdRowGraph:: LcdRowGraph(
-	LiquidCrystal& lcd,
-	int32_t min,
-	int32_t max,
-	uint8_t row,
-	uint8_t firstColumn,
-	uint8_t lastColumn)
+    LiquidCrystal& lcd,
+    int32_t min,
+    int32_t max,
+    uint8_t row,
+    uint8_t firstColumn,
+    uint8_t lastColumn)
 :
-	_lcd(lcd),
-	_min(min),
-	_max(max),
-	_row(row),
-	_firstColumn(firstColumn),
-	_columns(lastColumn - firstColumn + 1),
-	_setup(false)
+    _lcd(lcd),
+    _min(min),
+    _max(max),
+    _row(row),
+    _firstColumn(firstColumn),
+    _columns(lastColumn - firstColumn + 1),
+    _setup(false)
 {
 }
 
@@ -36,58 +36,69 @@ LcdRowGraph:: LcdRowGraph(
 void
 LcdRowGraph:: setup()
 {
-	for (uint8_t letter = 0 ; letter < 5 ; ++letter)
-	{
-		byte pixelRow = 0;
+    for (uint8_t letter = 0 ; letter < 5 ; ++letter)
+    {
+        byte pixelRow = 0;
 
-		for (int16_t bit = 5 ; bit > 4 - letter ; --bit)
-		{
-			bitSet(pixelRow, bit-1);
-		}
+        for (int16_t bit = 5 ; bit > 4 - letter ; --bit)
+        {
+            bitSet(pixelRow, bit-1);
+        }
 
-		byte pixels[8];
+        byte pixels[8];
 
-		for (uint8_t i = 0 ; i < 8 ; ++i)
-		{
-			pixels[i] = pixelRow;
-		}
+        for (uint8_t i = 0 ; i < 8 ; ++i)
+        {
+            pixels[i] = pixelRow;
+        }
 
-		_lcd.createChar(letter, pixels);
-	}
+        _lcd.createChar(letter, pixels);
+    }
 
-	_setup = true;
+    _setup = true;
 }
 
 //-------------------------------------------------------------------------
 
 void
 LcdRowGraph:: unscaled(
-	uint8_t value)
+    uint8_t value)
 {
-	if (not _setup)
-	{
-		setup();
-	}
+    if (not _setup)
+    {
+        setup();
+    }
 
-	uint8_t letters = value / 5;
-	uint8_t bar = value % 5;
-	uint8_t letter = 0;
+    uint8_t letters = value / 5;
+    uint8_t bar = value % 5;
+    uint8_t letter = 0;
 
-	_lcd.setCursor(letter + _firstColumn, _row);
+    _lcd.setCursor(letter + _firstColumn, _row);
 
-	for ( ; letter < letters ; ++letter)
-	{
-		_lcd.write((uint8_t)4);
-	}
+    for ( ; letter < letters ; ++letter)
+    {
+        _lcd.write((uint8_t)4);
+    }
 
     if (bar)
-	{
-	    ++letter;
-		_lcd.write(bar-1);
-	}
+    {
+        ++letter;
+        _lcd.write(bar-1);
+    }
 
-	for ( ; letter < _columns ; ++letter)
-	{
-		_lcd.print(" ");
-	}
+    for ( ; letter < _columns ; ++letter)
+    {
+        _lcd.print(" ");
+    }
+}
+
+//-------------------------------------------------------------------------
+
+void
+LcdRowGraph:: setColumns(
+    uint8_t first,
+    uint8_t last)
+{
+    _firstColumn = first;
+    _columns = last - first + 1;
 }
--- LcdRowGraph.h
+++ LcdRowGraph.h
@@ -31,39 +31,40 @@ class LcdRowGraph
 {
 public:
 
-	LcdRowGraph(
-		LiquidCrystal& lcd,
-		int32_t min,
-		int32_t max,
-		uint8_t row,
-		uint8_t firstColumn = 0,
-		uint8_t lastColumn = 15);
+    LcdRowGraph(
+        LiquidCrystal& lcd,
+        int32_t min,
+        int32_t max,
+        uint8_t row,
+        uint8_t firstColumn = 0,
+        uint8_t lastColumn = 15);
 
-	void value(int32_t value)
-	{
-		unscaled(map(value, _min, _max, 0, _columns * 5));
-	}
+    void value(int32_t value)
+    {
+        unscaled(map(value, _min, _max, 0, _columns * 5));
+    }
 
-	void unscaled(uint8_t value);
+    void unscaled(uint8_t value);
 
-	uint8_t getRow() const { return _row; }
+    uint8_t getRow() const { return _row; }
 
-	int32_t getMin() const { return _min; }
-	int32_t getMax() const { return _max; }
+    int32_t getMin() const { return _min; }
+    int32_t getMax() const { return _max; }
 
-	void setMinMax(int32_t min, int32_t max) { _min = min; _max = max; }
+    void setColumns(uint8_t first, uint8_t last);
+    void setMinMax(int32_t min, int32_t max) { _min = min; _max = max; }
 
 private: 
 
-	LiquidCrystal& _lcd;
-	int32_t _min;
-	int32_t _max;
-	uint8_t _row;
-	uint8_t _firstColumn;
-	uint8_t _columns;
-	boolean _setup;
+    LiquidCrystal& _lcd;
+    int32_t _min;
+    int32_t _max;
+    uint8_t _row;
+    uint8_t _firstColumn;
+    uint8_t _columns;
+    boolean _setup;
 
-	void setup();
+    void setup();
 };
 
 //-------------------------------------------------------------------------
